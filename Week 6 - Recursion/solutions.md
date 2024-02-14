bf93cec3d9a63f5bbaf5cea11201a28e23040b06
Ibrahim Chehab (IbraTech04) <chehab.ibrahim@outlook.com>
2024-02-14 14:20:17 -0500

    Updated solutions

---
+++ Week 6 - Recursion/solutions.md
@@ -0,0 +1,11 @@
+# Week Six Recap - Recursion 
+
+This week, we talked about the key terms that you should add to your UltraSheets:tm: for future midterm prep. We also discussed the practice problems found in slides.pdf
+
+## Solutions: 
+All solutions can be found in the [solutions.py](solutions.py) file, paired with descriptive comments and writeups for each problem. 
+
+## Hint for the homework question
+If you're struggling with the homework question, before looking at the solution try reading the following
+
+Recall in MAT102, we defined an even number as a number in the form $$2n, n \in \mathbb{Z}$$ and an odd number as a number in the form $$2n+1, n \in \mathbb{Z}$$. Consider the amount of times we subtract from the number to reach 0. **If that number is odd, the number is odd. If that number is even, the number is even.** Can we keep track of how many times we subtract from the number using recursion? 
--- Week 6 - Recursion/solutions.py
+++ Week 6 - Recursion/solutions.py
@@ -18,20 +18,28 @@ def deep_copy(lst: Union[int, list]) -> Union[int, list]:
     
     
 def ibranatchi_iterative(n: int) -> int:
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
-  elif n == 2:
-    return 5
+    # BASE CASES: These are copied verbatim to the recursive solutio
+    # Sometimes base cases in iterative methods aren't as clear, so you'll need to employ partial tracing to figure those out 
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 5
+    
+    # Pay attention to the pattern in the iterative solution. We're using the same pattern in the recursive solution, just in reverse :p
+    sequence = [0, 1, 5]
+    for i in range(3, n + 1):
+        next_value = sequence[i - 1] * 2 * sequence[i - 2] - 5 * sequence[i - 3]
+        sequence.append(next_value) # Each time, we're appending the next value to the sequence. The sequence will hold all the elements from 0 to n 
+        # Of the Ibranatchi sequence, and we can return the last element.
+        # This was done this way to make the question easier; some midterm qeustions will have a,b,c variables that swap and change, and you will have to keep track of them.
+        
+        # The sequence list is an "iterative" representation of the call stack in the recursive solution. If you notice, the recursive solution is doing the same thing but in reverse with functino calls. Instead of working it's way up, it's working it's way down while delegating the work to the next function call.
 
-  sequence = [0, 1, 5]
-  for i in range(3, n + 1):
-    next_value = sequence[i - 1] * 2 * sequence[i - 2] - 5 * sequence[i - 3]
-    sequence.append(next_value)
+    return sequence[-1]
 
-  return sequence[-1]
- 
 def ibranatchi_recursive(n: int) -> int:
     if n == 0:
         return 0
@@ -47,18 +55,24 @@ def ibranatchi_recursive(n: int) -> int:
     
     # With all this info, we get: 
     return 2 * ibranatchi_recursive(n - 1) * ibranatchi_recursive(n - 2) - 5 * ibranatchi_recursive(n - 3)
+    # Remember the analogy: Recursion does the minimum amount of work to solve the problem in each recursive fall before delegating the rest of the work to the next recursive call. 
+    # Here, we're working our way down to the base case, then through the method call stack we're working our way back up to the original call.
+    # This is the *inverse* of the iterative solution, which works it's way up to the final value from the base case.
 
 
 # DO NOT SCROLL DOWN UNLESS YOU WANT TO SEE THE SOLUTION FOR THE BONUS QUESTION
 # I DO NOT SUGGEST LOOKING AT THE ANSWER BEFORE TRYING TO SOLVE THE QUESTION YOURSELF
 
+# Hint for the bonus question: 
+# Recall in MAT102 how we defined an even number as: 2k, k \in Z and an odd number as 2k + 1, k \in Z
+
 
 def is_odd(n: int) -> bool:
-    if n == 0:
+    if n == 0: # Base case
         return False
-    return is_even(n - 1)
+    return is_even(n - 1) 
 
 def is_even(n: int) -> bool:
-    if n == 0:
+    if n == 0: # Base case
         return True
-    return is_odd(n - 1)
+    return is_odd(n - 1) 
